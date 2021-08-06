e5098d2b9a452ccd3d8e813802a8b57fca10907d
Seb <sebwiggins@me.com>
2021-08-06 10:46:40 +0100

    backend HTTP operations completed

--- Server/server.ts
+++ Server/server.ts
@@ -23,17 +23,33 @@ const app = express();
 app.use(express.json()); //add body parser to each following route handler
 app.use(cors()) //add CORS support to each following route handler
 
-const client = new Client(dbConfig);
+const client = new Client({user: "academy",
+password: "",
+host: "localhost",
+port: 5432,
+database: "pastebinback",});
 client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
+  const dbres = await client.query('select * from pastebintext');
+  res.json(dbres.rows);
+});
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params
+  const dbres = await client.query('select * from pastebintext WHERE id = $1', [id]);
+  res.json(dbres.rows[0]);
+});
+
+app.post("/newpost", async (req, res) => {
+  const { text, title } = req.body
+  const dbres = await client.query('insert into pastebintext (text, title) VALUES($1, $2)', [text, title]);
   res.json(dbres.rows);
 });
 
 
 //Start the server on the given port
-const port = process.env.PORT;
+const port = process.env.PORT;  
 if (!port) {
   throw 'Missing PORT environment variable.  Set it in .env file.';
 }
